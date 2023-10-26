db22e73c839dab8f587c7b2070e37a5d7727ac03
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-10-26 15:16:48 -0300

    second commit

--- cypress/e2e/contact-book/interacoes.cy.js
+++ cypress/e2e/contact-book/interacoes.cy.js
@@ -15,20 +15,17 @@ describe('testar inclusão de item', () => {
     cy.get('input[type="email"]').type('gianprofessor@ebac.com.br');
     cy.get('button[type="submit"]').click();
 
-    cy.get('.contato').should('have.length.gt', 1); // Verifique se há 1 item na lista.
+    cy.get('.contato').should('have.length.gt', 1); 
   })
 
   it('deve verificar a exclusao de um item', () => {
-    //cy.get('input[type="text"]').type('gian Souza');
-    //cy.get('input[type="tel"]').type('11912345678');
-    //cy.get('input[type="email"]').type('gian@ebac.com.br');
     cy.get('.contato:first .delete').click();
-    //cy.get('.contato:first').should('have.length', 0); // Verifique se há 1 item na lista.
+
   })
 });
-describe('testar exclusao de item', () => {
+describe('testar edição de item', () => {
 it('deve verificar a edição de itens', () => {
-  // Suponhamos que você queira editar o primeiro item na lista.
+
   cy.get('.contato:eq(1) .edit').click();
   cy.get('input[type="text"]').clear().type('Novo Nome');
   cy.get('input[type="tel"]').clear().type('999999999');
